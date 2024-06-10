4eefbc70f43ef2448f46f73787b28ea5d4443ee3
Prusia117 <124855521+Prusia117@users.noreply.github.com>
2024-06-10 07:20:25 +0000

    Added my changes!

--- src/icp_hello_world_motoko_backend/main.mo
+++ src/icp_hello_world_motoko_backend/main.mo
@@ -1,5 +1,145 @@
+import HashMap "mo:base/HashMap";
+import Text "mo:base/Text";
+import Float "mo:base/Float";
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+
+/********* Tipos de datos perzonalizados *********/ 
+
+  // Tipo de dato de entrada
+  type InputDataType = {
+    //titleInput : Float;
+    lengthInput : Float;
+    widthInput : Float;
+    heightInput : Float;
+    enduranceInput : Text;
   };
+
+  // Tipo de dato de salida
+  type outDataType = {
+    //tittle : Text;
+    mcubic : Float;
+    wbucket : Float;
+    gbucket : Float;
+    ssand : Float;
+    scement : Float;
+  };
+
+  // Tipo de dato de equivalencias de materiales
+  type EquivalencesType = {
+    wbucket : Float;
+    gbucket : Float;
+    ssand : Float;
+    scement : Float;
+  };
+
+/********* HashMap *********/ 
+
+  // Base de datos de equivalencias basada en HashMap
+  let equivalencesDB = HashMap.HashMap<Text, EquivalencesType>(5, Text.equal, Text.hash);
+
+  // Agragando equivalencias al HashMap
+  equivalencesDB.put(
+    "endurance100",
+    {
+      wbucket = 12.5;
+      gbucket = 40;
+      ssand = 35;
+      scement = 5;
+    },
+  );
+
+  equivalencesDB.put(
+    "endurance150",
+    {
+      wbucket = 12;
+      gbucket = 39;
+      ssand = 33;
+      scement = 6;
+    },
+  );
+
+  equivalencesDB.put(
+    "endurance200",
+    {
+      wbucket = 12.25;
+      gbucket = 38.5;
+      ssand = 31.5;
+      scement = 7;
+    },
+  );
+
+  equivalencesDB.put(
+    "endurance250",
+    {
+      wbucket = 12;
+      gbucket = 40;
+      ssand = 28;
+      scement = 8;
+    },
+  );
+
+  equivalencesDB.put(
+    "endurance300",
+    {
+      wbucket = 11.5;
+      gbucket = 36;
+      ssand = 22.5;
+      scement = 9;
+    },
+  );
+
+  /********* Declaración de variablaes *********/ 
+  var _cmeters : Float = 0;
+
+  var _outData : outDataType = {
+    //tittle = "";
+    mcubic = 0;
+    wbucket = 0;
+    gbucket = 0;
+    ssand = 0;
+    scement = 0;
+  };
+
+  /********* Funciones *********/ 
+
+  // Función para obtener las equivalencias de materiales mediante un codigo de resistencia
+  public func getEquivalences(_input : InputDataType) : async ?EquivalencesType {
+    return equivalencesDB.get(_input.enduranceInput);
+  };
+
+  // Función para calcular los metros cúbicos con la formula Altura x Ancho x Largo
+  public func calculateCubicMeters(_input : InputDataType) : async Float {
+    return _input.lengthInput * _input.widthInput * _input.heightInput;
+  };
+
+  // Función para calcular la dosificación de concreto con la formula metros cúbicos x equivalencias
+  public func calculateDosage(_input : InputDataType) : async ?outDataType {
+
+    //Calculando los metros cúbicos
+    _cmeters := await calculateCubicMeters(_input);
+
+    //Obteniendo las equivalencias de materiales mediante el codigo de resistencia
+    let _equivalences = await getEquivalences(_input);
+
+
+    switch (_equivalences) {
+      case (?e) {
+
+        //Calculando la dosificación
+        _outData := {
+          //tittle = "Lot Calculation";
+          mcubic = _cmeters;
+          wbucket = _cmeters * e.wbucket;
+          gbucket = _cmeters * e.gbucket;
+          ssand = _cmeters * e.ssand;
+          scement = _cmeters * e.scement;
+        };
+      };
+      case (null) return null;
+    };
+
+    //Retornando la dosificación
+    return ?_outData;
+  };
+
 };
--- src/icp_hello_world_motoko_frontend/src/index.js
+++ src/icp_hello_world_motoko_frontend/src/index.js
@@ -1,19 +1,272 @@
 import { icp_hello_world_motoko_backend } from "../../declarations/icp_hello_world_motoko_backend";
+import './bootstrap.bundle.min.js';
+import './bootstrap.js';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+document.addEventListener('DOMContentLoaded', function () {
 
-  const name = document.getElementById("name").value.toString();
+  /* Selectores */
+  const form = document.getElementById('form');
+  const lengthInput = document.getElementById('lengthInput');
+  const widthInput = document.getElementById('widthInput');
+  const heightInput = document.getElementById('heightInput');
+  const enduranceInput = document.getElementById('enduranceInput');
+  const submit = document.getElementById('submit');
+  const loaderDiv = document.querySelector('.loader-div');
 
-  button.setAttribute("disabled", true);
+  const endurance = document.querySelector('.endurance');
+  const mcubic = document.querySelector('.mcubic');
+  const wbucket = document.querySelector('.wbucket');
+  const sbucket = document.querySelector('.sbucket');
+  const gbucket = document.querySelector('.gbucket');
+  const scement = document.querySelector('.scement');
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await icp_hello_world_motoko_backend.greet(name);
+  /* Clases */
+  class UI {
 
-  button.removeAttribute("disabled");
+    outAlert(message, divReference) {
 
-  document.getElementById("greeting").innerText = greeting;
+      this.deleteAlert(divReference);
 
-  return false;
+      const pAlert = document.createElement('P');
+      pAlert.classList.add('alert', 'alert-danger', 'msg-alert', 'mt-3', 'text-center');
+      pAlert.textContent = message;
+
+      divReference.appendChild(pAlert);
+
+    }
+
+    deleteAlert(reference) {
+      const alert = reference.querySelector('.msg-alert');
+      if (alert) {
+        alert.remove();
+      }
+    }
+
+    alertCheck = () => form.querySelectorAll('.msg-alert').length > 0;
+
+    roundNumber(number) {
+      return Math.round(number * 10000) / 10000;
+    }
+
+    showDosage(outData) {
+
+      if (outData[0]) {
+
+        mcubic.textContent = " " + this.roundNumber(outData[0].mcubic);
+        wbucket.textContent = " " + this.roundNumber(outData[0].wbucket);
+        scement.textContent = " " + this.roundNumber(outData[0].scement);
+        sbucket.textContent = " " + this.roundNumber(outData[0].ssand);
+        gbucket.textContent = " " + this.roundNumber(outData[0].gbucket);
+
+      }
+
+    }
+
+    resetShowDosage() {
+
+      endurance.textContent = " ";
+      mcubic.textContent = " ";
+      scement.textContent = " ";
+      wbucket.textContent = " ";
+      sbucket.textContent = " ";
+      gbucket.textContent = " ";
+
+    }
+
+    resetForm() {
+      form.reset();
+    }
+
+    readModeForm() {
+
+      lengthInput.readOnly = !lengthInput.readOnly;
+      widthInput.readOnly = !widthInput.readOnly;
+      heightInput.readOnly = !heightInput.readOnly;
+      enduranceInput.readOnly = !enduranceInput.readOnly;
+
+    }
+
+  }
+
+  const ui = new UI();
+
+  class ValidateData {
+
+    getEquivalenceCode = (equivalence) => {
+
+      let code = "";
+
+      switch (equivalence) {
+        case "0":
+          code = "error";
+          break;
+        case "1":
+          code = "endurance100";
+          break;
+        case "2":
+          code = "endurance150";
+          break;
+        case "3":
+          code = "endurance200";
+          break;
+        case "4":
+          code = "endurance250";
+          break;
+        default:
+          code = "endurance300";
+          break;
+      }
+
+      return code;
+    }
+
+    getEndurance = (enduranceInput) => {
+
+      let enduranceEquivalence = "";
+
+      switch (enduranceInput) {
+        case "0":
+          enduranceEquivalence = "error";
+          break;
+        case "1":
+          enduranceEquivalence = "Concreto 100 Kg/Cm² Grava ¾";
+          break;
+        case "2":
+          enduranceEquivalence = "Concreto 150 Kg/Cm² Grava ¾";
+          break;
+        case "3":
+          enduranceEquivalence = "Concreto 200 Kg/Cm² Grava ¾";
+          break;
+        case "4":
+          enduranceEquivalence = "Concreto 250 Kg/Cm² Grava ¾";
+          break;
+        default:
+          enduranceEquivalence = "Concreto 300 Kg/Cm² Grava ¾";
+          break;
+      }
+
+      return enduranceEquivalence;
+    }
+
+    isNumber(n) {
+      return /^-?\d+(\.\d+)?$/.test(n);
+    }
+
+    validateInput(e) {
+
+
+      const alertCheckRes = ui.alertCheck();
+
+      if (e.target.value.trim() === "") {
+
+        const msg = "La cantidad es obligatorio";
+
+        ui.outAlert(msg, e.target.parentElement);
+        submit.disabled = true;
+        return;
+      }
+
+      inputs.forEach(input => {
+        if (e.target.id === input && !this.isNumber(e.target.value.trim())) {
+
+          ui.outAlert("Ingresa una cantidad valida", e.target.parentElement);
+          submit.disabled = true;
+          return;
+        }
+      });
+
+      if (e.target.id === "enduranceInput" && e.target.value.trim() === "0") {
+        ui.outAlert("La recistencia es obligatoria", e.target.parentElement);
+        submit.disabled = true;
+        return;
+      }
+
+
+      if (lengthInput.value.trim() !== "" && widthInput.value.trim() !== "" && heightInput.value.trim() !== "" && enduranceInput.value.trim() !== "0" && !alertCheckRes) {
+        submit.disabled = false;
+      }
+
+    }
+
+  }
+
+  const validateData = new ValidateData();
+
+  /* Variables */
+
+  const inputs = ["lengthInput", "widthInput", "heightInput"];
+
+
+  /*  Eventos */
+
+  function eventListeners() {
+    lengthInput.addEventListener('input', inputValidation);
+    widthInput.addEventListener('input', inputValidation);
+    heightInput.addEventListener('input', inputValidation);
+    enduranceInput.addEventListener('input', inputValidation);
+
+    form.addEventListener('submit', submitData);
+  }
+
+  eventListeners();
+
+  function inputValidation(e) {
+    ui.deleteAlert(e.target.parentElement);
+    validateData.validateInput(e);
+
+    const alertCheckRes = ui.alertCheck();
+
+    if (lengthInput.value.trim() !== "" && widthInput.value.trim() !== "" && heightInput.value.trim() !== "" && enduranceInput.value.trim() !== "0" && !alertCheckRes) {
+
+      submit.disabled = false;
+
+    } else {
+
+      submit.disabled = true;
+
+    }
+  }
+
+  async function submitData(e) {
+    e.preventDefault();
+
+    ui.resetShowDosage();
+    ui.readModeForm();
+    loaderDiv.hidden = !loaderDiv.hidden;
+    submit.disabled = true;
+
+    const outData = {
+      lengthInput: parseFloat(lengthInput.value),
+      widthInput: parseFloat(widthInput.value),
+      heightInput: parseFloat(heightInput.value),
+      enduranceInput: validateData.getEquivalenceCode(enduranceInput.value)
+    };
+
+
+    try {
+
+      const calculateDosage = await icp_hello_world_motoko_backend.calculateDosage(outData);
+
+
+
+      if (calculateDosage) {
+
+        setTimeout(function () {
+          endurance.textContent = validateData.getEndurance(enduranceInput.value);
+          ui.readModeForm();
+          ui.resetForm();
+          ui.showDosage(calculateDosage);
+          submit.disabled = true;
+          loaderDiv.hidden = !loaderDiv.hidden;
+        }, 2000);
+
+      }
+
+    } catch (error) {
+
+      console.error("A ocurrido un error al calcular la Dosificación: ", error);
+
+    }
+  }
+
 });
